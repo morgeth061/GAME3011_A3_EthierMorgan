bcfd26d637c176deec61a3bdb7c1512bc091bfb8
Morgan Ethier <morganethier@gmail.com>
2022-03-19 13:19:44 -0400

    Initial commit

---
+++ Assets/[Scripts]/TitleScreenManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScreenManager : MonoBehaviour
+{
+    public void OnAssignment1ButtonClick()
+    {
+        SceneManager.LoadScene("Assignment1");
+    }
+
+    public void OnAssignment2ButtonClick()
+    {
+        SceneManager.LoadScene("Assignment2");
+    }
+}
